907194a43fc662921e114ea7cf485d014cf4fda0
Aman <paman7681@gmail.com>
2023-01-07 18:08:57 +0530

    commit3

---
+++ frontend/src/Components/Widgets/Dive/Dive.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MessageIcon from '../images/message.png'
+
+export default function Dive() {
+  return (
+    <div className='flex flex-row h-1/2 mt-10 rounded-md shadow-md p-2 w-1/2  '>
+        <img src={MessageIcon}/>
+    </div>
+  )
+}
